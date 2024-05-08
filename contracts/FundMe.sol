4f07ae5a27401045c706be333f1a7568d1eefaa1
0xVaibhav <guptavaibhav0240@gmail.com>
2024-05-09 01:27:25 +0530

    fundme.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,3 +1,39 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+ error NotOwner();
+contract FundMe {
+   mapping (address => uint256)addressToAmountFunded;
+
+   address [] public funders;
+   address public immutable i_owner;
+   uint256 public constant MINIMUM_USD = 5*10*8;
+
+   constructor () {
+    i_owner =  msg.sender ;
+   }
+
+   function Fund () public payable {
+    require(msg.value <= MINIMUM_USD , "not enough eth sent ");
+    addressToAmountFunded[msg.sender] += msg.value;
+    funders.push(msg.sender);
+   }
+
+   function getversion () public view returns(uint256){
+    AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    return pricefeed.version();
+   }
+
+   modifier OnlyOwner{
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+   }
+
+   function withdraw() public OnlyOwner{
+    
+
+   }
+
+}
